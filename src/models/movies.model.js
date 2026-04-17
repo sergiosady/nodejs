b4b289bb7b378bd30ada3400976f3e87b002654b
Sergio Sady <sergiosadymf@gmail.com>
2026-04-17 19:06:58 -0300

    refatorando

--- src/models/movies.model.js
+++ src/models/movies.model.js
@@ -3,11 +3,11 @@ import { v4 as UUID } from "uuid";
 export default class Movie {
   constructor({ title, description, year, genre, image, video }) {
     this.id = UUID();
-    this.title = title;
-    this.description = description;
-    this.year = year;
-    this.genre = genre;
-    this.image = image;
-    this.video = video;
+    this.title = title || "Título Genérico";
+    this.description = description || "Sem descrição";
+    this.year = year || "Desconhecido";
+    this.genre = genre || null;
+    this.image = image || `${title}.img`;
+    this.video = video || `${title}.mp4`;
   }
 }
